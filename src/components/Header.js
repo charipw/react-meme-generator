db7d762c0b15c5b7bc9869184c591fa9ff24d622
Charleen Ipping Wah <charleen.ipping@gmail.com>
2023-03-06 11:13:53 +0000

    Amend header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,10 @@ import trollFace from "../images/troll-face.png";
 
 export default function Header() {
   return (
-    <header className="header--container">
-      <div className="header--left--side">
-        <img src={trollFace} className="header--img" />
-        <h1>Meme Generator</h1>
-      </div>
-      <p>React Course - Project 3</p>
+    <header className="header">
+      <img src={trollFace} className="header--image" />
+      <h2 className="header--title">Meme Generator</h2>
+      <h4 className="header--project">React Course - Project 3</h4>
     </header>
   );
 }
