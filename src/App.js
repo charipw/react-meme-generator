5b9b3f4311c4afbf2d5421fbc5419b33a6855b56
Charleen Ipping Wah <charleen.ipping@gmail.com>
2023-03-06 11:34:53 +0000

    Create and design form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./components/Header";
 import trollFace from "./images/troll-face.png";
+import Meme from "./components/Meme";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Meme />
     </div>
   );
 }
