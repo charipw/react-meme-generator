fdeb1a679120a4c06f3b1bdef9db9112d3c25a5d
Charleen Ipping Wah <charleen.ipping@gmail.com>
2023-03-06 13:29:30 +0000

    Create meem generator and event listener

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,13 +1,24 @@
 import React from "react";
+import memeData from "../memeData";
 
 export default function Meme() {
+  function memeGenerator() {
+    // console.log("You clicked!")
+    const memeArray = memeData.data.memes;
+    const memeIndex = Math.floor(Math.random() * memeArray.length);
+    const url = memeArray[memeIndex].url;
+    console.log(url);
+  }
+
   return (
     <main>
-      <form className="form">
+      <div className="form">
         <input type="text" placeholder="Top text" className="form--input" />
         <input type="text" placeholder="Bottom text" className="form--input" />
-        <button className="form--button">Get a new meme image 🖼</button>
-      </form>
+        <button onClick={memeGenerator} className="form--button">
+          Get a new meme image 🖼
+        </button>
+      </div>
     </main>
   );
 }
