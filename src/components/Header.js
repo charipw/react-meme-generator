4db3b06f57f60007d80916daced6ef5886063c32
Charleen Ipping Wah <charleen.ipping@gmail.com>
2023-04-19 12:44:46 +0100

    Add alt to images in Header.js and Meme.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import trollFace from "../images/troll-face.png";
 export default function Header() {
   return (
     <header className="header">
-      <img src={trollFace} className="header--image" />
+      <img src={trollFace} alt={"troll face logo"} className="header--image" />
       <h2 className="header--title">Meme Generator</h2>
       <h4 className="header--project">React Course - Project 3</h4>
     </header>
